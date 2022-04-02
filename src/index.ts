c9adb1ca20d41bd201ae29dec77c9a1f2eaa6c14
Sugam Adhikari <sugam.adhiakri123@gmail.com>
2022-04-02 12:59:12 +0545

    Removed implementation of static html frontend

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as http from "http";
 import { AuthorService } from "./services/AuthorService";
+import { BookService } from "./services/BookService";
+import { MagazineService } from "./services/MagazineService";
+import { ReadableService } from "./services/ReadableService";
 
 const authorService = AuthorService.instance;
+const bookService = BookService.instance;
+const magazineService = MagazineService.instance;
+const readableservice = ReadableService.instance;
 
---
+++ src/services/ReadableService.ts
@@ -0,0 +1,45 @@
+import { Readable } from "../Models/Readable";
+import { Book } from "../Models/Book";
+import { Magazine } from "../Models/Magazine";
+import { BookService } from "./BookService";
+import { MagazineService } from "./MagazineService";
+
+export class ReadableService {
+    private static _instance: ReadableService;
+
+    private readables: Readable[];
+    private bookService: BookService;
+    private magazineService: MagazineService;
+
+
+    private constructor() {
+        this.bookService = BookService.instance;
+        this.magazineService = MagazineService.instance;
+
+        // Setup readables
+        const books: Book[] = this.bookService.getAllBooks();
+        const magazines: Magazine[] = this.magazineService.getAllMagazines();
+        this.readables = [...books, ...magazines];
+    }
+
+    public static get instance(): ReadableService {
+        if (ReadableService._instance == null) {
+            ReadableService._instance = new ReadableService();
+        }
+
+        return ReadableService._instance;
+    }
+
+    public getBooksAndMagazineSortedByTitle() {
+
+        const sortedBooksAndMagazines = this.readables.sort((s1, s2) => {
+            if (s1 > s2) return 1;
+            if (s1 < s2) return -1;
+            return 0
+        });
+
+        return sortedBooksAndMagazines;
+    }
+
+
+}
